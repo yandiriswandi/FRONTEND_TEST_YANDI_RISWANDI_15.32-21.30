75df80239af2f89451dd92b9f458c998f2204904
Yandi Riswandi <yandiriswandi@gmail.com>
2022-08-23 20:12:28 +0700

    1.3

--- Soal No. 1/src/components/Form.js
+++ Soal No. 1/src/components/Form.js
@@ -1,24 +1,18 @@
 // import React Bootstrap components here
 import { Form, Row, Col, Dropdown, DropdownButton} from 'react-bootstrap'
-
-// import css modules
-import cssModules from "./Form.module.css";
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-
-
 
 function FormComponent() {
   const [provinsi, setProvinsi] = useState([]);
   const [provinsiId, setProvinsiId] = useState('');
   const [kota, setKota] = useState([]);
+  const [kotaId, setKotaId] = useState('');
   const [kecamatan, setKecamatan] = useState([]);
-
- console.log(provinsiId);
 
 const styles = {
   form: {
-    margin: "16px 20% 0",
+    margin: "100px 20% 0",
   },
 };
 
@@ -39,89 +33,95 @@ function fetchDataProvinsi() {
 }
 
 
-
-function fetchDataKecamatan() {
-  axios
-    .get(
-      'https://dev.farizdotid.com/api/daerahindonesia/kecamatan?id_kota=%7Bid_kota%7D'
-    )
-    .then((res) => {
-      setKecamatan(res.data);
-      // console.log(res.data);
-    })
-    .catch((err) => {
-      console.log('error', err);
-    });
-}
-
-function fetchDataKota(provinsiId) {
+function fetchDataKota() {
   axios
   .get(
     `https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=${provinsiId}`
-    // 'https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi='+ provinsiId
     )
     .then((res) => {
-      setKota(res.data);
-      console.log(res.data);
+      setKota(res.data.kota_kabupaten);
+      console.log(res.data.kota_kabupaten);
     })
     .catch((err) => {
       console.log('error', err);
     });
   }
+
+function fetchDataKecamatan() {
+  axios
+    .get(
+      `https://dev.farizdotid.com/api/daerahindonesia/kecamatan?id_kota=${kotaId}`
+    )
+    .then((res) => {
+      setKecamatan(res.data.kecamatan);
+      console.log(res.data.kecamatan);
+    })
+    .catch((err) => {
+      console.log('error', err);
+    });
+  }
+
   useEffect(() => {
     fetchDataProvinsi();
-  }, [provinsiId]);
+  }, []);
   useEffect(() => {
     fetchDataKota();
-  }, [provinsiId]);
+  }, [provinsiId,kotaId]);
   useEffect(() => {
     fetchDataKecamatan();
-  }, []);
+  }, [kotaId]);
   
   const handleProvinsi=(event)=>{
+      const getprovinsiId = event.target.value;
+      setProvinsiId(getprovinsiId); 
+  } 
+
+  const handleKota=(event)=>{
    
-    const getprovinsiId = event.target.value;
- 
-  setProvinsiId(getprovinsiId); 
-} 
+    const getkotaId = event.target.value;
+    console.log(getkotaId);
+    setKotaId(getkotaId); 
+  } 
+
   return (
-    <div>
+    <div className='c'>
       <form style={styles.form}>
         <div className="form-group">
           <label htmlFor="gender" className="form-label">
             Provinsi 
           </label>
           {/* <Select options={provinsi} /> */}
-          <select id="provinsi" name="provinsi" className="form-select" onChange={(e)=>handleProvinsi(e)}>
-          <option>Select Provinsi</option>
+          <select id="provinsi" name="provinsi" className="form-select shadow" onChange={(e)=>handleProvinsi(e)}>
+            <option className='text-option'>Select Provinsi</option>
           {provinsi?.map((item,index) => (
             <option key={item.id} value={item.id}>{item.nama}</option>
-            ))}
+          ))}
           </select>
         </div>
 
-        <div className="form-group">
+        <div className="form-group mt-4">
           <label htmlFor="gender" className="form-label">
             Kota
           </label>
-          <select id="gender" className="form-select" defaultValue="Choose...">
-          {/* {kota?.map((item,index) => (
-            <option>{item.nama}</option>
-            ))} */}
+          <select id="kota" name="kota" className="form-select shadow" onChange={(e)=>handleKota(e)}>
+            <option className='text-option'>Select Kota / Kabupaten</option>
+          {kota?.map((item,index) => (
+            <option key={item.id} value={item.id}>{item.nama}</option>
+          ))}
           </select>
         </div>
 
-        <div className="form-group">
+        <div className="form-group mt-4">
           <label htmlFor="gender" className="form-label">
            Kecamatan
           </label>
-          <select id="gender" className="form-select" defaultValue="Choose...">
-          {provinsi?.map((item,index) => (
-            <option>{item.kecamatan}</option>
-            ))}
+          <select id="gender" className="form-select shadow" defaultValue="Choose...">
+            <option className='text-option'>Select Kecamatan</option>
+          {kecamatan?.map((item,index) => (
+            <option>{item.nama}</option>
+          ))}
           </select>
         </div>
-
       </form>
     </div>
   )
